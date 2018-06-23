47b2da5b88ff0592d24f8aba414cdb8609406fdf
HerculesGit <hercules.silva@dce.ufpb.br>
2018-06-23 15:27:05 -0300

    Modificação de iterador

--- Queue.py
+++ Queue.py
@@ -15,6 +15,7 @@ class Queue:
     def dequeue(self):
         'remove um elemento no começo da fila e retorna ele'
         if len(self.q) == 0:
+	    #raise é uma palavra-chave que levanta a instrução colocada pelo construtor
             raise EmptyQueueError(' remoção de uma fila vazia')
         return self.q.pop(0)
 
@@ -33,7 +34,37 @@ class Queue:
     def __repr__(self):
         return '{}'.format(self.q)
 
+    def __getitem__(self,chave):
+        'com isso poderemos usar o operador de indexaçao []'
+        #agora tbm poderemos usar o for-each
+        return self.q[chave]
+    
+    def __setitem__(self,chave,item):
+        self.q[chave] = item
+
+    def __iter__(self):
+        'retorn iterador de Queue'
+        return QueueIterator(self)
+
 
 class EmptyQueueError(Exception):
     pass
 
+class QueueIterator:
+    'iterador para a classe de contêiner Queue'
+
+    def __init__(self,q):
+        'costrutor'
+        self.indice = len(q)-1
+        self.q = q
+
+    def __next__(self):
+        '''retorna o próximo item de Queue; se não houver item
+            seguinte, levanta exceção StopIteration '''
+        if self.indice < 0:
+            raise StopIteration()
+
+        res = self.q[self.indice]
+        self.indice -=1
+        return res
+        
